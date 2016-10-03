1c320e71787e72e182248b8912f369cd09193fae
Ally Boyd <alyssa.boyd85@gmail.com>
2016-10-03 16:22:15 -0500

    Finished code challenge

--- app.js
+++ app.js
@@ -4,14 +4,34 @@ $(document).ready(function () {
   var yellowCount = 0;
   var greenCount = 0;
   var blueCount = 0;
-  var $appendCube = function () { $('.container').append('<div id="color-cube"></div>');
-};
+  var $appendCube = function () { $('.container').append('<div class="color-cube"></div>');
+  };
   $('button').on('click', function () {
-    if ($('button').data('color') == 'red') {
+    if ($(this).data('color') == 'red') {
       redCount++;
-      $appendCube;
-      $('.color-cube'
+      $appendCube();
+      $('.container').children().last().addClass('red');
+      $('#red').replaceWith('<p id="red">Total red: ' + redCount + '</p>');
       console.log(redCount);
+    } else if ($(this).data('color') == 'yellow') {
+      yellowCount++;
+      $appendCube();
+      $('.container').children().last().addClass('yellow');
+      $('#yellow').replaceWith('<p id="yellow">Total yellow: ' + yellowCount + '</p>');
+      console.log(yellowCount);
+    } else if ($(this).data('color') == 'green') {
+      greenCount++;
+      $appendCube();
+      $('.container').children().last().addClass('green');
+      $('#green').replaceWith('<p id="green">Total green: ' + greenCount + '</p>');
+      console.log(greenCount);
+    } else if ($(this).data('color') == 'blue') {
+      blueCount++;
+      $appendCube();
+      $('.container').children().last().addClass('blue');
+      $('#blue').replaceWith('<p id="blue">Total blue: ' + blueCount + '</p>');
+      console.log(blueCount);
     }
   });
+
 });
